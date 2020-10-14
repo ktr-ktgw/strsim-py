ca08e9dcd09467583b03d2f421cf8a16e4883f5b
Kotaro Kitagawa <kitagawa@miraitranslate.com>
2020-10-14 14:48:46 +0900

    add levenshtein distance algorithm

--- src/levenshtein.py
+++ src/levenshtein.py
@@ -1,4 +1,21 @@
 def levenshtein_distance(s1, s2):
+    ld = [[0] * (len(s2)+1) for i in range(len(s1)+1)]
+    ld[0] = range(len(s2)+1)
+
+    for i in range(1, len(s1)+1):
+        ld[i][0] = i
+        for j in range(1, len(s2)+1):
+            diff = 0 if s1[i-1] == s2[j-1] else 1
+            ld[i][j] = min(
+                ld[i-1][j] + 1,
+                ld[i][j-1] + 1,
+                ld[i-1][j-1] + diff
+            )
+    return ld[len(s1)][len(s2)]
+
+
+
+def levenshtein_distance_recursive(s1, s2):
     def lev(a, b, i, j):
         if i == 0: return j
         elif j == 0: return i
--- tests/test_levenshtein.py
+++ tests/test_levenshtein.py
@@ -1,5 +1,5 @@
 import unittest
-from src.levenshtein import levenshtein_distance
+from src.levenshtein import levenshtein_distance, levenshtein_distance_recursive
 
 class TestLevenshtein(unittest.TestCase):
 
@@ -13,6 +13,15 @@ class TestLevenshtein(unittest.TestCase):
         self.assertEqual(levenshtein_distance('hogefuga', 'huga'), 4)
         self.assertEqual(levenshtein_distance('これはこれはすばらしい', 'これなんだい'), 8)
 
+    def test_levenshtein_equal_rec(self):
+        self.assertEqual(levenshtein_distance_recursive('hoge', 'hoge'), 0)
+        self.assertEqual(levenshtein_distance_recursive('あ亜', 'あ亜'), 0)
+
+    def test_levenshtein_diff_rec(self):
+        self.assertEqual(levenshtein_distance_recursive('hoge', 'fuga'), 3)
+        self.assertEqual(levenshtein_distance_recursive('琴稲妻', '琴光喜'), 2)
+        self.assertEqual(levenshtein_distance_recursive('hogefuga', 'huga'), 4)
+        self.assertEqual(levenshtein_distance_recursive('これはこれはすばらしい', 'これなんだい'), 8)
 
 if __name__ == '__main__':
     unittest.main()
