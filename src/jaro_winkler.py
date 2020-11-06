cc22bfbc02d8b084c52179de0e6b6c58d23eeb3d
Kotaro Kitagawa <kitagawa@miraitranslate.com>
2020-11-07 04:40:30 +0900

    add jaro similarity algorithm

---
+++ src/jaro_winkler.py
@@ -0,0 +1,35 @@
+from math import floor
+from collections import defaultdict
+
+def jaro_similarity(s1, s2):
+    if s1 == s2:
+        return 1.0
+    max_dist = floor(max(len(s1),len(s2))/2) -1
+    
+    # m is the number of matching characters
+    m = 0
+    match_s1 = defaultdict(lambda: False)
+    match_s2 = defaultdict(lambda: False)
+    for i in range(len(s1)):
+        for j in range(max(0, i - max_dist), min(len(s2), i + max_dist + 1)):
+            if s1[i] == s2[j]:
+                match_s1[i] = True
+                match_s2[j] = True
+                m += 1
+                break
+    
+    if (m == 0): 
+        return 0.0
+    
+    # t is half the number of transpositions
+    t = 0
+    pos = 0
+    for i in range(len(s1)): 
+        if (match_s1[i]): 
+            while (not match_s2[pos]): 
+                pos += 1
+            if (s1[i] != s2[pos]): 
+                pos += 1
+                t += 1
+    t //= 2
+    return (m/len(s1) + m/len(s2) + (m-t+1)/m)/ 3.0
---
+++ tests/test_jaro_winkler.py
@@ -0,0 +1,17 @@
+import unittest
+from src.jaro_winkler import jaro_similarity
+
+class TestJaroWinkler(unittest.TestCase):
+
+    def test_jaro_equal(self):
+        self.assertEqual(jaro_similarity('hoge', 'hoge'), 1)
+        self.assertEqual(jaro_similarity('あ亜', 'あ亜'), 1)
+
+    def test_jaro_diff(self):
+        self.assertEqual(jaro_similarity('MARTHA', 'MARHTA'), 0.9444444444444445)
+        self.assertEqual(jaro_similarity('DIXON', 'DICKSONX'), 0.766667)
+        self.assertEqual(jaro_similarity('MARTHA', 'hoge'), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
